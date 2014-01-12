19d9349cdfc7d8d7469f1cd2f79623c1b25b0764
prafullgangboir <prafull.gangboir@gmail.com>
2014-01-12 18:55:32 -0500

    Update helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,17 +1,22 @@
+// StringManip.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include<string>
 #include<iostream>
 
-
-template<typename T>
-void display(const T& val)
+int main()
 {
-	std::cout<<val<<"\n";
-}
-
-
-
-
-template<typename T>
-void displaywithtab(const T& val)
-{
-	std::cout<<val<<"\t";
+	std::string s;
+	s = "cs106";
+	std::string s2 = "Prafull";
+	s.replace(0,1,s2);
+	std::cout<<s<<std::endl;
+	std::cout<<s2<<std::endl;
+	std::string line;
+	std::cin>>line;
+	std::cout<<line<<std::endl;
+	for (int i = 0; i < s.length(); i++)
+		s[i] = toupper(s[i]);
+	std::cout<<"String is :"<<s<<std::endl;
 }
