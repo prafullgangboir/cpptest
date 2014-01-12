0818836578bcd37a57c8733c3376233b8854b630
prafullgangboir <prafull.gangboir@gmail.com>
2014-01-12 23:23:08 +0530

    Create helloworld.cpp

---
+++ helloworld.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+
+template<typename T>
+void display(const T& val)
+{
+	std::cout<<val<<"\n";
+}
+
+
+
+
+template<typename T>
+void displaywithtab(const T& val)
+{
+	std::cout<<val<<"\t";
+}
